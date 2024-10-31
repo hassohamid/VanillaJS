2ae86bc7284eb7a29bcf9f2ddf123f7fc97bf809
hassohamid <hassohamid@hotmail.com>
2024-10-31 19:28:57 +0100

    score functionality

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -1,1 +1,32 @@
-'use strict';
+const secretNumber = Math.floor(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "❌ No Number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number! ";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too High!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = " 😥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 Too Low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = " 😥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
